fc2891aa9d3743ecbcc2c837a6c364a0d58e6746
Cỏ Khô <92889822+HuynhCoKho@users.noreply.github.com>
2026-06-03 22:36:07 +0700

    Move quick links into config

--- config.js
+++ config.js
@@ -1,3 +1,35 @@
 window.SBV_FAQ_CONFIG = {
-  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx-PdqyxSXY097q7dLB9mXJOxKmH9Aps9gxMiR7e1JQflvF8SFUy2I6Zhbnt-qORUG6_g/exec'
+  APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx-PdqyxSXY097q7dLB9mXJOxKmH9Aps9gxMiR7e1JQflvF8SFUy2I6Zhbnt-qORUG6_g/exec',
+  TOPIC_LINKS: [
+    {
+      title: 'Cho vay ra nước ngoài và thu hồi nợ bảo lãnh cho người không cư trú',
+      url: 'https://notebooklm.google.com/notebook/e8292942-6691-4ea1-942a-6d62abb2a5c5'
+    },
+    {
+      title: 'Đầu tư gián tiếp ra nước ngoài',
+      url: 'https://notebooklm.google.com/notebook/681b4b29-7e61-4044-b938-8f05ba382ee2'
+    },
+    {
+      title: 'Vay trả nợ nước ngoài',
+      url: 'https://notebooklm.google.com/notebook/934c2e58-63f8-407b-b48b-80e85e11e5b9'
+    },
+    {
+      title: 'Mở tài khoản ngoại tệ ở nước ngoài',
+      url: 'https://notebooklm.google.com/notebook/7da77be0-9b77-499f-8f35-effbb42fb41d'
+    },
+    {
+      title: 'Phát hành trái phiếu quốc tế',
+      url: 'https://notebooklm.google.com/notebook/1988ee47-df81-4e69-8377-d28faeeb29ec'
+    }
+  ],
+  TOOL_LINKS: [
+    {
+      title: 'Xác định khoản vay nước ngoài dưới hình thức nhập khẩu hàng hóa trả chậm',
+      url: 'https://script.google.com/macros/s/AKfycbzgh_ub-qfPIMxqw-dA82dH_M9EUzJpYTjtoUf9U0mhNU7pL9_lTJlgDdlK1lR5oaPfBA/exec'
+    },
+    {
+      title: 'Từ điển cá nhân',
+      url: 'https://huynhcokho.github.io/Tudiencanhan/'
+    }
+  ]
 };
