8b25bb261c736f20a83e817e36d330747ff632fd
Cỏ Khô <92889822+HuynhCoKho@users.noreply.github.com>
2026-06-03 23:50:04 +0700

    Sync fallback sidebar links

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SBV_FAQ_CONFIG = {
   APPS_SCRIPT_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbx-PdqyxSXY097q7dLB9mXJOxKmH9Aps9gxMiR7e1JQflvF8SFUy2I6Zhbnt-qORUG6_g/exec',
   TOPIC_LINKS: [
-   {
+    {
       title: 'Vay trả nợ nước ngoài',
       url: 'https://notebooklm.google.com/notebook/934c2e58-63f8-407b-b48b-80e85e11e5b9'
     },
@@ -9,7 +9,7 @@ window.SBV_FAQ_CONFIG = {
       title: 'Cho vay ra nước ngoài và thu hồi nợ bảo lãnh cho người không cư trú',
       url: 'https://notebooklm.google.com/notebook/e8292942-6691-4ea1-942a-6d62abb2a5c5'
     },
- {
+    {
       title: 'Đầu tư trực tiếp ra nước ngoài',
       url: 'https://notebooklm.google.com/notebook/75209b11-a7a6-4e0e-9d93-a98d99ef273e'
     },
@@ -17,7 +17,6 @@ window.SBV_FAQ_CONFIG = {
       title: 'Đầu tư gián tiếp ra nước ngoài',
       url: 'https://notebooklm.google.com/notebook/681b4b29-7e61-4044-b938-8f05ba382ee2'
     },
-   
     {
       title: 'Mở tài khoản ngoại tệ ở nước ngoài',
       url: 'https://notebooklm.google.com/notebook/7da77be0-9b77-499f-8f35-effbb42fb41d'
